6c1d0d59898bd30088a4ea432c5da969dc05b110
Nguyễn Hoàng Tân <83364365+hoangtan12314@users.noreply.github.com>
2024-02-29 15:30:17 +0700

    Add many line Checkout main and fix this file

--- rebase.js
+++ rebase.js
@@ -1,3 +1,12 @@
 function rebase() {
     console.log("Testing rebase")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+    console.log("Checkout main and fix this file")
+
 }
